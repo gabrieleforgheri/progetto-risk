a5f3b550e1e4cb41d6aa12880990ecce47f803c6
Marcello Pandolfi <357912@studenti.unimore.it>
2026-04-17 14:11:10 +0200

    Create Territory class

---
+++ baseIA/models/Territory.java
@@ -0,0 +1,10 @@
+package models;
+
+public class Territory {
+    private int armies;
+    private String owner;
+    private List<Territory> neighbors;
+    private String continent;
+
+    // constructor, getters, and setters
+}
